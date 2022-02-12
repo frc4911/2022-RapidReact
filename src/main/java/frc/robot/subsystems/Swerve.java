20528205c981ea4867471b9e13bc2e315d39e2d5
Matthew <mchida57@gmail.com>
2022-02-12 14:28:58 -0800

    Pigeon 2.0 implementation: No BasePigeon class; will need to add flexibility if needed, but it functions properly for 2022 robot

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -49,7 +49,7 @@ public class Swerve extends Subsystem {
 	double lastUpdateTimestamp = 0;
 
 	// Swerve kinematics & odometry
-	private final Pigeon mPigeon;
+	private final PigeonTwo mPigeon;
 	private Rotation2d mGyroOffset = Rotation2d.identity();
 
 	private final SwerveDriveOdometry mOdometry;
@@ -87,7 +87,7 @@ public class Swerve extends Subsystem {
         int m3 = 0;
 
 
-        mPigeon = Pigeon.getInstance();
+        mPigeon = PigeonTwo.getInstance();
 
         if (RobotName.name.equals(Constants.kJuniorName)) {
             mSwerveConfiguration = Junior.kSwerveConfiguration;
@@ -112,7 +112,7 @@ public class Swerve extends Subsystem {
         }
 
         mKinematics = new SwerveDriveKinematics(mSwerveConfiguration.moduleLocations);
-		mOdometry = new SwerveDriveOdometry(mKinematics, mPigeon.getYaw());
+		mOdometry = new SwerveDriveOdometry(mKinematics, mPigeon.getRotation2dYaw());
         mPeriodicIO.robotPose = mOdometry.getPose();
 
         mMotionPlanner = new DriveMotionPlanner();
@@ -266,7 +266,7 @@ public class Swerve extends Subsystem {
      * @return The angle of the robot (CCW).
      */
     private synchronized Rotation2d getAngle() {
-        return mPigeon.getYaw();
+        return mPigeon.getRotation2dYaw();
     }
 
     public Pose2d getPose() {
@@ -283,7 +283,7 @@ public class Swerve extends Subsystem {
      * @param pose The (x,y,thetha) position.
      */
     public synchronized void setRobotPosition(Pose2d pose) {
-        mOdometry.resetPosition(pose, mPigeon.getYaw());
+        mOdometry.resetPosition(pose, mPigeon.getRotation2dYaw());
         mPeriodicIO.robotPose = mOdometry.getPose();
     }
 
@@ -452,7 +452,7 @@ public class Swerve extends Subsystem {
         double now                   = Timer.getFPGATimestamp();
         mPeriodicIO.schedDeltaActual = now - mPeriodicIO.lastSchedStart;
         mPeriodicIO.lastSchedStart   = now;
-        mPeriodicIO.gyro_heading     = Rotation2d.fromDegrees(mPigeon.getYaw().getDegrees()).rotateBy(mGyroOffset);
+        mPeriodicIO.gyro_heading     = Rotation2d.fromDegrees(mPigeon.getRotation2dYaw().getDegrees()).rotateBy(mGyroOffset);
 
         // read modules
         mModules.forEach((m) -> m.readPeriodicInputs());
