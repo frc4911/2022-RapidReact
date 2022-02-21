1599c6e5afd1a7ccd5078db6b438729916de9356
Kevin Emery <360emery@gmail.com>
2022-02-21 11:16:56 -0800

    Refactor robot-specific consants into constants in config classes
    This originally started with me noticing that the PIDF values
    were duplicated across all 4 modules in each class and morphed
    into me removing some of the magic numbers into constants at
    the top of the file.

--- src/main/java/frc/robot/config/Junior.java
+++ src/main/java/frc/robot/config/Junior.java
@@ -12,6 +12,21 @@ import static frc.robot.Constants.kMK2_WheelDiameter;
 
 public class Junior implements RobotConfiguration {
 
+    private static final double FRONT_RIGHT_STEER_MOTOR_ENCODER_HOME_OFFSET = 883.0;
+    private static final double FRONT_LEFT_STEER_MOTOR_ENCODER_HOME_OFFSET = 1683.0;
+    private static final double BACK_LEFT_STEER_MOTOR_ENCODER_HOME_OFFSET = 3451.0;
+    private static final double BACK_RIGHT_STEER_MOTOR_ENCODER_HOME_OFFSET = -327.0;
+
+    private static final int FRONT_RIGHT_CANCODER_OFFSET_DEGREES = 80;
+    private static final int FRONT_LEFT_CANCODER_OFFSET_DEGREES = -28;
+    private static final int BACK_LEFT_CANCODER_OFFSET_DEGREES = 146;
+    private static final int BACK_RIGHT_CANCODER_OFFSET_DEGREES = -24;
+
+    private static final double STEER_MOTOR_KP = 0.4;
+    private static final double STEER_MOTOR_KI = 0.0;
+    private static final double STEER_MOTOR_KD = 0.0;
+    private static final double STEER_MOTOR_KF = 0.0;
+
     @Override
     public SwerveConfiguration getSwerveConfiguration() {
         return new SwerveConfiguration(
@@ -31,16 +46,16 @@ public class Junior implements RobotConfiguration {
         frontRightModuleConstants.kModuleId = 0;
         frontRightModuleConstants.kDriveMotorTalonId = Ports.FRONT_RIGHT_DRIVE;
         frontRightModuleConstants.kSteerMotorTalonId = Ports.FRONT_RIGHT_STEER;
-        frontRightModuleConstants.kSteerMotorEncoderHomeOffset = 883.0;
-        frontRightModuleConstants.kCANCoderOffsetDegrees = 80;
+        frontRightModuleConstants.kSteerMotorEncoderHomeOffset = FRONT_RIGHT_STEER_MOTOR_ENCODER_HOME_OFFSET;
+        frontRightModuleConstants.kCANCoderOffsetDegrees = FRONT_RIGHT_CANCODER_OFFSET_DEGREES;
         frontRightModuleConstants.kCANCoderId = Ports.FRONT_RIGHT_CANCODER;
         frontRightModuleConstants.kWheelDiameter = kMK2_WheelDiameter;
         frontRightModuleConstants.kDriveReduction = (18.0 / 38.0) * (26.0 / 18.0) * (15.0 / 60.0);
         frontRightModuleConstants.kSteerReduction = kMK2_SteerReduction;
-        frontRightModuleConstants.kSteerMotorSlot0Kp = 0.4;
-        frontRightModuleConstants.kSteerMotorSlot0Ki = 0.0;
-        frontRightModuleConstants.kSteerMotorSlot0Kd = 0.0;
-        frontRightModuleConstants.kSteerMotorSlot0Kf = 0.0;
+        frontRightModuleConstants.kSteerMotorSlot0Kp = STEER_MOTOR_KP;
+        frontRightModuleConstants.kSteerMotorSlot0Ki = STEER_MOTOR_KI;
+        frontRightModuleConstants.kSteerMotorSlot0Kd = STEER_MOTOR_KD;
+        frontRightModuleConstants.kSteerMotorSlot0Kf = STEER_MOTOR_KF;
 
         return frontRightModuleConstants;
     }
@@ -53,16 +68,16 @@ public class Junior implements RobotConfiguration {
         frontLeftModuleConstants.kModuleId = 1;
         frontLeftModuleConstants.kDriveMotorTalonId = Ports.FRONT_LEFT_DRIVE;
         frontLeftModuleConstants.kSteerMotorTalonId = Ports.FRONT_LEFT_STEER;
-        frontLeftModuleConstants.kSteerMotorEncoderHomeOffset = 1683.0;
-        frontLeftModuleConstants.kCANCoderOffsetDegrees = -28;
+        frontLeftModuleConstants.kSteerMotorEncoderHomeOffset = FRONT_LEFT_STEER_MOTOR_ENCODER_HOME_OFFSET;
+        frontLeftModuleConstants.kCANCoderOffsetDegrees = FRONT_LEFT_CANCODER_OFFSET_DEGREES;
         frontLeftModuleConstants.kCANCoderId = Ports.FRONT_LEFT_CANCODER;
         frontLeftModuleConstants.kWheelDiameter = kMK2_WheelDiameter;
         frontLeftModuleConstants.kDriveReduction = (18.0 / 38.0) * (26.0 / 18.0) * (15.0 / 60.0);
         frontLeftModuleConstants.kSteerReduction = kMK2_SteerReduction;
-        frontLeftModuleConstants.kSteerMotorSlot0Kp = 0.4;
-        frontLeftModuleConstants.kSteerMotorSlot0Ki = 0.0;
-        frontLeftModuleConstants.kSteerMotorSlot0Kd = 0.0;
-        frontLeftModuleConstants.kSteerMotorSlot0Kf = 0.0;
+        frontLeftModuleConstants.kSteerMotorSlot0Kp = STEER_MOTOR_KP;
+        frontLeftModuleConstants.kSteerMotorSlot0Ki = STEER_MOTOR_KI;
+        frontLeftModuleConstants.kSteerMotorSlot0Kd = STEER_MOTOR_KD;
+        frontLeftModuleConstants.kSteerMotorSlot0Kf = STEER_MOTOR_KF;
 
         return frontLeftModuleConstants;
     }
@@ -75,16 +90,16 @@ public class Junior implements RobotConfiguration {
         backLeftModuleConstants.kModuleId = 2;
         backLeftModuleConstants.kDriveMotorTalonId = Ports.BACK_LEFT_DRIVE;
         backLeftModuleConstants.kSteerMotorTalonId = Ports.BACK_LEFT_STEER;
-        backLeftModuleConstants.kSteerMotorEncoderHomeOffset = 3451.0;
-        backLeftModuleConstants.kCANCoderOffsetDegrees = 146;
+        backLeftModuleConstants.kSteerMotorEncoderHomeOffset = BACK_LEFT_STEER_MOTOR_ENCODER_HOME_OFFSET;
+        backLeftModuleConstants.kCANCoderOffsetDegrees = BACK_LEFT_CANCODER_OFFSET_DEGREES;
         backLeftModuleConstants.kCANCoderId = Ports.BACK_LEFT_CANCODER;
         backLeftModuleConstants.kWheelDiameter = kMK2_WheelDiameter;
         backLeftModuleConstants.kDriveReduction = (18.0 / 38.0) * (26.0 / 18.0) * (15.0 / 60.0);
         backLeftModuleConstants.kSteerReduction = kMK2_SteerReduction;
-        backLeftModuleConstants.kSteerMotorSlot0Kp = 0.4;
-        backLeftModuleConstants.kSteerMotorSlot0Ki = 0.0;
-        backLeftModuleConstants.kSteerMotorSlot0Kd = 0.0;
-        backLeftModuleConstants.kSteerMotorSlot0Kf = 0.0;
+        backLeftModuleConstants.kSteerMotorSlot0Kp = STEER_MOTOR_KP;
+        backLeftModuleConstants.kSteerMotorSlot0Ki = STEER_MOTOR_KI;
+        backLeftModuleConstants.kSteerMotorSlot0Kd = STEER_MOTOR_KD;
+        backLeftModuleConstants.kSteerMotorSlot0Kf = STEER_MOTOR_KF;
 
         return backLeftModuleConstants;
     }
@@ -97,16 +112,16 @@ public class Junior implements RobotConfiguration {
         backRightModuleConstants.kModuleId = 3;
         backRightModuleConstants.kDriveMotorTalonId = Ports.BACK_RIGHT_DRIVE;
         backRightModuleConstants.kSteerMotorTalonId = Ports.BACK_RIGHT_STEER;
-        backRightModuleConstants.kSteerMotorEncoderHomeOffset = -327.0;
+        backRightModuleConstants.kSteerMotorEncoderHomeOffset = BACK_RIGHT_STEER_MOTOR_ENCODER_HOME_OFFSET;
         backRightModuleConstants.kCANCoderId = Ports.BACK_RIGHT_CANCODER;
-        backRightModuleConstants.kCANCoderOffsetDegrees = -24;
+        backRightModuleConstants.kCANCoderOffsetDegrees = BACK_RIGHT_CANCODER_OFFSET_DEGREES;
         backRightModuleConstants.kWheelDiameter = kMK2_WheelDiameter;
         backRightModuleConstants.kDriveReduction = (18.0 / 38.0) * (26.0 / 18.0) * (15.0 / 60.0);
         backRightModuleConstants.kSteerReduction = kMK2_SteerReduction;
-        backRightModuleConstants.kSteerMotorSlot0Kp = 0.4;
-        backRightModuleConstants.kSteerMotorSlot0Ki = 0.0;
-        backRightModuleConstants.kSteerMotorSlot0Kd = 0.0;
-        backRightModuleConstants.kSteerMotorSlot0Kf = 0.0;
+        backRightModuleConstants.kSteerMotorSlot0Kp = STEER_MOTOR_KP;
+        backRightModuleConstants.kSteerMotorSlot0Ki = STEER_MOTOR_KI;
+        backRightModuleConstants.kSteerMotorSlot0Kd = STEER_MOTOR_KD;
+        backRightModuleConstants.kSteerMotorSlot0Kf = STEER_MOTOR_KF;
 
         return backRightModuleConstants;
     }
