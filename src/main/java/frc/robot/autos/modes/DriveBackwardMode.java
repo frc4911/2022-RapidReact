a9ee3fbaf192cfc1390c3367a01cc861739cbd67
Matthew <mchida57@gmail.com>
2022-03-02 18:42:13 -0800

    Added forward and backward driving auto modes. No flexible drive distances

---
+++ src/main/java/frc/robot/autos/modes/DriveBackwardMode.java
@@ -0,0 +1,13 @@
+package frc.robot.autos.modes;
+
+import frc.robot.actions.DriveTrajectoryAction;
+import frc.robot.paths.TrajectoryGenerator;
+import libraries.cheesylib.autos.AutoModeBase;
+import libraries.cheesylib.autos.AutoModeEndedException;
+
+public class DriveBackwardMode extends AutoModeBase{
+    @Override
+    protected void routine() throws AutoModeEndedException {
+       runAction(new DriveTrajectoryAction(TrajectoryGenerator.getInstance().getTrajectorySet().backwardTrajectory.left));
+    }
+}
