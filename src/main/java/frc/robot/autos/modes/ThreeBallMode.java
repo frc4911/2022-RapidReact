a8b65969b713185c2ab396e4bedc0951230ffb83
johansu <johansu@msn.com>
2022-03-29 07:57:01 -0700

    Apply startpose as last step of auto

--- src/main/java/frc/robot/autos/modes/ThreeBallMode.java
+++ src/main/java/frc/robot/autos/modes/ThreeBallMode.java
@@ -1,19 +1,17 @@
 package frc.robot.autos.modes;
 
-import java.util.Arrays;
-
 import frc.robot.actions.CollectAction;
 import frc.robot.actions.DriveTrajectoryAction;
-import frc.robot.actions.EndTrajectoryAction;
 import frc.robot.actions.ManualShootAction;
 import frc.robot.actions.SetPoseAction;
 import frc.robot.paths.TrajectoryGenerator;
 import libraries.cheesylib.autos.AutoModeBase;
 import libraries.cheesylib.autos.AutoModeEndedException;
-import libraries.cheesylib.autos.actions.ParallelAction;
 import libraries.cheesylib.autos.actions.WaitAction;
 import libraries.cheesylib.geometry.Pose2d;
 import libraries.cheesylib.geometry.Rotation2d;
+import libraries.cheesylib.geometry.Translation2d;
+import libraries.cheesylib.util.Units;
 
 public class ThreeBallMode extends AutoModeBase{
     @Override
@@ -26,7 +24,10 @@ public class ThreeBallMode extends AutoModeBase{
         // collect
         // shoot
         // done
-    
+
+        Pose2d startPose = new Pose2d(new Translation2d(Units.inches_to_meters(-18.5), Units.inches_to_meters(-48.2)),
+                Rotation2d.fromDegrees(69));
+
         runAction(new ManualShootAction(3, 5));
         runAction(new CollectAction(true));
         runAction(new DriveTrajectoryAction(TrajectoryGenerator.getInstance().getTrajectorySet().threeBallAuto0Trajectory.left, false));
@@ -35,7 +36,6 @@ public class ThreeBallMode extends AutoModeBase{
         runAction(new CollectAction(false));
         runAction(new DriveTrajectoryAction(TrajectoryGenerator.getInstance().getTrajectorySet().threeBallAuto2Trajectory.left, false));
         runAction(new ManualShootAction(15, 5));
-        runAction(new SetPoseAction(new Pose2d(0,0,new Rotation2d(119.0))));
-
+        runAction(new SetPoseAction(new Pose2d(0,0,new Rotation2d(119.0)), true));
     }
 }
