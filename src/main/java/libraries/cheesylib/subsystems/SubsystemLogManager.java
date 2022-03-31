cc7e0f5cad4cdfc2eb21025f16158426e1248fc6
RedDogFang <kingbros@msn.com>
2022-03-30 21:56:23 -0700

    added code to copy log from rio to usb device on power up

--- src/main/java/libraries/cheesylib/subsystems/SubsystemLogManager.java
+++ src/main/java/libraries/cheesylib/subsystems/SubsystemLogManager.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ConcurrentLinkedDeque;
@@ -130,6 +131,9 @@ public class SubsystemLogManager {
                 }
             }
         }
+
+        moveCompletedLogsToUSB(mBasePath+mCompletedLogs);
+
     }
 
     public boolean checkAvailableLogSpace() {
@@ -164,6 +168,40 @@ public class SubsystemLogManager {
             }
         }
         return fileSizes;
+    }
+
+    private void moveCompletedLogsToUSB(String path){
+        String[] pathnames;
+        File dir = new File(path);
+
+        if (dir.exists()){
+            pathnames = dir.list();
+
+            if (pathnames != null){
+                for (String filename: pathnames){
+                    File file = new File(dir.toString()+"/"+filename);
+                    File dest = new File("/U/"+filename);
+                    long fileSize = file.length();
+                    try {
+                        Files.copy(file.toPath(), dest.toPath());
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    System.out.println("moved "+file.toString() +" to "+ dest.toString());
+
+                    if (dest.exists() && dest.length()==fileSize) {
+                        file.delete();
+                    }
+                }
+            }
+            else{
+                System.out.println("no log files to move");
+            }
+        }
+        else{
+            System.out.println("did not find usb drive");
+        }
     }
 
     // append data from one subsystem to StringBuilder
