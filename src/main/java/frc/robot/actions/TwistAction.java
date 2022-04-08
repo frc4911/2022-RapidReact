919211026aed8052e11e61646ba4c24e42af74d2
Caleb West <calebewest@outlook.com>
2022-04-08 11:22:40 -0700

    Add back old tested code for twisting during auto period

--- src/main/java/frc/robot/actions/TwistAction.java
+++ src/main/java/frc/robot/actions/TwistAction.java
@@ -7,36 +7,21 @@ public class TwistAction implements Action {
 
 	private String sClassName;
 	private Swerve mSwerve;
-	private double heading; // Assume the start heading is 0 - var is the desired heading 0-360
-	private boolean direction; // CCW is true, CW is false
+	private double heading;
+	private boolean direction; // CW is true, CCW is false
+	private double target;
 	private double speed;
 
-	/**
-	 *  Independent twist action for swerve during auto. Allows the robot to rotate (in place)
-	 *  until a certain heading is achieved. Can rotate either clockwise or counterclockwise.
-	 *  Normalizes angles to 0-360, where positive 90 is counterclockwise, directly to the left
-	 *  @param heading The desired heading to twist to
-	 *  @param direction The direction of the twist (true/counterclockwise, false/clockwise)
-	 */
-		
-	public TwistAction(double heading, boolean direction) { //If direction changes within one automode it will cause the heading variable vs. actual heading to be off-caleb
+	public TwistAction(double heading, boolean direction) {
 		sClassName = this.getClass().getSimpleName();
 		mSwerve = Swerve.getInstance(sClassName);
-		heading %= 360;
-		if (heading < 0) {
-			heading += 360;
-		}
-		this.heading = heading;
+		this.heading = Math.abs(heading);
 		this.direction = direction;
 	}
 
 	@Override
 	public boolean isFinished() {
-		double normalizedHeading = (mSwerve.getHeading().getDegrees() % 360);
-		if (normalizedHeading < 0) {
-			normalizedHeading += 360;
-		}
-		if (Math.abs(heading - normalizedHeading) <= 10) {
+		if (Math.abs(target - mSwerve.getHeading().getDegrees()) <= 10.0) {
 			return true;
 		}
 		return false;
@@ -45,9 +30,11 @@ public class TwistAction implements Action {
 	@Override
 	public void start() {
 		if (direction) {
-			speed = 0.7;
+			target = mSwerve.getHeading().getDegrees() - heading;
+			speed = -0.75;
 		} else {
-			speed = -0.7;
+			target = mSwerve.getHeading().getDegrees() + heading;
+			speed = 0.75;
 		}
 	}
 
@@ -60,4 +47,4 @@ public class TwistAction implements Action {
 	public void done() {
 		mSwerve.setTeleopInputs(0, 0, 0, false, false, false);
 	}
-}
+} 
